4c942cc45896382e19d25a9ba03d7db207910231
KabeloMaps <mpnkab004@myuct.ac.za>
2026-02-16 20:56:45 +0200

    added chapter, manga, mongo and postgres + config skeleton

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"blackedge-backend/database"
 	"blackedge-backend/routes"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-
-	database.ConnectPostgres()
-	database.ConnectMongo()
 
 	routes.RegisterRoutes(r)
 
--- config/config.go
+++ config/config.go
@@ -1,1 +1,24 @@
 package config
+
+import "os"
+
+type Config struct {
+	PostgresDSN string
+	MongoURI string
+	ServerPort string
+}
+
+func LoadConfig() *Config{
+	return &Config{
+		PostgresDSN: getEnv("POSTGRES_DSN", "host-localhost user=postgres password=postgres dbname=blackedge port=5432 sslmode=disable"),
+		MongoURI: getEnv("MONGO_URI", "mongodb://localhost:27017"),
+		ServerPort: getEnv("PORT", "8080"),
+	}
+}
+
+func getEnv(key, fallback string) string{
+	if v := os.Getenv(key); v != ""{
+		return v
+	}
+	return fallback
+}
